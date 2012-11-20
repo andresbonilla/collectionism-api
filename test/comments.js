5264936d9f9685e580fee9c59361dfec007dbf56
Andres Bonilla <andresbonilla@gmail.com>
2012-11-20 00:51:22 -0500

    Added comment creation test for invalid attributes

--- test/comments.js
+++ test/comments.js
@@ -38,6 +38,30 @@ describe('Comment', function () {
             });
         });
         
+        it('does not create comment when given invalid attributes', function (done) {
+             helper.signedInUser(function(err, res, body) {
+                 var user = body.user;
+                 Factory.create('item', function(item) {
+                     helper.createComment({
+                         user: {
+                             _id: user._id,
+                             auth_token: user.auth_token
+                         },
+                         comment: {
+                             item_id: item._id,
+                             text: ''
+                         }
+                     },
+                     function (err, res, body) {
+                         res.statusCode.should.be.equal(400);
+                         body.error.message.should.equal('Validation failed');
+                         done();
+                     }); 
+                 });
+             });
+            
+         });
+        
     });
     
     
